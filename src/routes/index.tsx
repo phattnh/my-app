c9274256df750c7bc86e44a637cd7b13db8acd71
jayce <phathoang@incognito.org>
2020-01-14 22:56:04 +0700

    init project

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <p className='title'>Hello World!</p>
+    </div>
+  );
+};
+
+export default App;
